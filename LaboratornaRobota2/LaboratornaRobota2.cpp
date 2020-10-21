3ea0b398c344cc45ce3798f83e9b7f4a2cf36548
makson202007@gmail.com <makson202007@gmail.com>
2020-10-21 11:29:27 +0300

    new version

--- LaboratornaRobota2/LaboratornaRobota2.cpp
+++ LaboratornaRobota2/LaboratornaRobota2.cpp
@@ -2,10 +2,24 @@
 //
 
 #include <iostream>
+#include <math.h>
+#include <stdlib.h>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float x1, x2, y1, y2, l;
+    cout << "x1 = "; cin >> x1;
+    cout << "y1 = "; cin >> y1;
+    cout << "x2 = "; cin >> x2;
+    cout << "y2 = "; cin >> y2;
+    l = sqrt(pow((x2 - x1), 2) + (pow((y2 - y1), 2)));
+    printf("c= %1.3f\n ", l);
+
+    return 0;
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
